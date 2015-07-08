6189066ed0f587af8961ed3fbdd8214a75908ffd
Sebastien Badia <sbadia@redhat.com>
2015-07-08 17:32:36 +0200

    spec: Update deprecated rspec keywords
    Update RSpec 3.x keywords in order to remove deprecation
    warnings.
    
    Change-Id: Ibfd3c9f88c84be9d21746e880ecaa60bc646e7a5

--- spec/shared_examples.rb
+++ spec/shared_examples.rb
@@ -1,6 +1,6 @@
 shared_examples_for "a Puppet::Error" do |description|
   it "with message matching #{description.inspect}" do
-    expect { should have_class_count(1) }.to raise_error(Puppet::Error, description)
+    expect { is_expected.to have_class_count(1) }.to raise_error(Puppet::Error, description)
   end
 end
 
@@ -8,12 +8,12 @@ shared_examples 'generic sahara service' do |service|
 
   context 'with default parameters' do
     it 'installs package and service' do
-      should contain_package(service[:name]).with({
+      is_expected.to contain_package(service[:name]).with({
         :name   => service[:package_name],
         :ensure => 'present',
         :notify => ["Service[#{service[:name]}]"]
       })
-      should contain_service(service[:name]).with({
+      is_expected.to contain_service(service[:name]).with({
         :name      => service[:service_name],
         :ensure    => 'running',
         :hasstatus => true,
@@ -29,12 +29,12 @@ shared_examples 'generic sahara service' do |service|
     end
 
     it 'installs package and service' do
-      should contain_package(service[:name]).with({
+      is_expected.to contain_package(service[:name]).with({
         :name   => service[:package_name],
         :ensure => '2014.2-1',
         :notify => ["Service[#{service[:name]}]"]
       })
-      should contain_service(service[:name]).with({
+      is_expected.to contain_service(service[:name]).with({
         :name      => service[:service_name],
         :ensure    => 'running',
         :hasstatus => true,
@@ -50,7 +50,7 @@ shared_examples 'generic sahara service' do |service|
     end
 
     it 'does not control service state' do
-      should contain_service(service[:name]).without_ensure
+      is_expected.to contain_service(service[:name]).without_ensure
     end
   end
 end
