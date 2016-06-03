044636de27826421de2e2e52d89ffbf49fb36cc3
ZhongShengping <chdzsp@163.com>
2016-06-04 00:34:12 +0800

    Test multiple operating systems for policy
    Use on_supported_os function to test multiple
    operating systems for class sahara::policy.
    
    Change-Id: I405c21de5b20f1f2adba13474c8ba4067c69e0dd

--- spec/classes/sahara_policy_spec.rb
+++ spec/classes/sahara_policy_spec.rb
@@ -24,19 +24,16 @@ describe 'sahara::policy' do
     end
   end
 
-  context 'on Debian platforms' do
-    let :facts do
-      { :osfamily => 'Debian' }
-    end
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge(OSDefaults.get_facts({:processorcount => 8}))
+      end
 
-    it_configures 'sahara policies'
+      it_configures 'sahara policies'
+    end
   end
 
-  context 'on RedHat platforms' do
-    let :facts do
-      { :osfamily => 'RedHat' }
-    end
-
-    it_configures 'sahara policies'
-  end
 end
