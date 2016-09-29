e203d5b6f52611767815b174a7b4a9eff9809ff2
Sharat Sharma <sharat.sharma@nectechnologies.in>
2016-09-29 12:53:54 +0530

    Added retries for db_sync
    If db sync fails, it is never retried leading to errors. So, this
    patch adds retries for db_sync to avoid sync fails.
    
    Change-Id: Ib7162c07542d9c8c81ed35f931a2b91281610774
    Related-Bug: #1628580
    Closes-Bug: #1645474
    (cherry picked from commit 3f8e5d1c542dda2bc261c74fa1dad1d3289aa686)

--- manifests/db/sync.pp
+++ manifests/db/sync.pp
@@ -25,6 +25,8 @@ class sahara::db::sync(
     path        => '/usr/bin',
     user        => 'sahara',
     refreshonly => true,
+    try_sleep   => 5,
+    tries       => 10,
     logoutput   => on_failure,
     tag         => 'openstack-db',
   }
