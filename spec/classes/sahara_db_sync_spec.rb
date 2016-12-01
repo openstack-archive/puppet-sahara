5f5f1e75963ef9f09f8f2e926b625268f53bc377
ZhongShengping <chdzsp@163.com>
2016-12-01 17:45:51 +0800

    Fix spec test for sync
    Change-Id: Ic08a0187a8c25a4d3e470fe519ffb91cefa15148

--- spec/classes/sahara_db_sync_spec.rb
+++ spec/classes/sahara_db_sync_spec.rb
@@ -11,6 +11,8 @@ describe 'sahara::db::sync' do
           :path        => '/usr/bin',
           :user        => 'sahara',
           :refreshonly => 'true',
+          :try_sleep   => 5,
+          :tries       => 10,
           :logoutput   => 'on_failure',
           :tag         => 'openstack-db',
         )
@@ -30,6 +32,8 @@ describe 'sahara::db::sync' do
           :path        => '/usr/bin',
           :user        => 'sahara',
           :refreshonly => 'true',
+          :try_sleep   => 5,
+          :tries       => 10,
           :logoutput   => 'on_failure',
           :tag         => 'openstack-db',
         )
