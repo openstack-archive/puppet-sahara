91879287fc5acb2d158c765ec068689921523fc4
ZhongShengping <chdzsp@163.com>
2019-04-28 15:00:12 +0800

    Deprecate idle_timeout option
    The idle_timeout parameter is deprecated, use connection_recycle_time
    instead[1].
    
    [1]https://review.opendev.org/#/c/334182/
    
    Change-Id: I77af04663ce89551966889d8496ae6a0a1802d53
    Depends-On: https://review.opendev.org/656106/
    Closes-Bug: #1826692

--- manifests/db.pp
+++ manifests/db.pp
@@ -18,7 +18,7 @@
 #   Set to -1 to specify an infinite retry count.
 #   Defaults to $::os_service_default.
 #
-# [*database_idle_timeout*]
+# [*database_connection_recycle_time*]
 #   (Optional) Timeout before idle SQL connections are reaped.
 #   Defaults to $::os_service_default.
 #
@@ -42,42 +42,55 @@
 #   (Optional) If set, use this value for pool_timeout with SQLAlchemy.
 #   Defaults to $::os_service_default
 #
+# DEPRECATED PARAMETERS
+#
+# [*database_idle_timeout*]
+#   Timeout when db connections should be reaped.
+#   Defaults to undef.
+#
 class sahara::db (
-  $database_db_max_retries = $::os_service_default,
-  $database_connection     = 'mysql+pymysql://sahara:secrete@localhost:3306/sahara',
-  $database_idle_timeout   = $::os_service_default,
-  $database_min_pool_size  = $::os_service_default,
-  $database_max_pool_size  = $::os_service_default,
-  $database_max_retries    = $::os_service_default,
-  $database_retry_interval = $::os_service_default,
-  $database_max_overflow   = $::os_service_default,
-  $database_pool_timeout   = $::os_service_default,
+  $database_db_max_retries          = $::os_service_default,
+  $database_connection              = 'mysql+pymysql://sahara:secrete@localhost:3306/sahara',
+  $database_connection_recycle_time = $::os_service_default,
+  $database_min_pool_size           = $::os_service_default,
+  $database_max_pool_size           = $::os_service_default,
+  $database_max_retries             = $::os_service_default,
+  $database_retry_interval          = $::os_service_default,
+  $database_max_overflow            = $::os_service_default,
+  $database_pool_timeout            = $::os_service_default,
+  # DEPRECATED PARAMETERS
+  $database_idle_timeout            = undef,
 ) {
 
   include ::sahara::deps
 
+  if $database_idle_timeout {
+    warning('The database_idle_timeout parameter is deprecated. Please use \
+database_connection_recycle_time instead.')
+  }
+
   # NOTE(degorenko): In order to keep backward compatibility we rely on the pick function
   # to use sahara::<myparam> if sahara::db::<myparam> isn't specified.
-  $database_connection_real     = pick($::sahara::database_connection, $database_connection)
-  $database_idle_timeout_real   = pick($::sahara::database_idle_timeout, $database_idle_timeout)
-  $database_min_pool_size_real  = pick($::sahara::database_min_pool_size, $database_min_pool_size)
-  $database_max_pool_size_real  = pick($::sahara::database_max_pool_size, $database_max_pool_size)
-  $database_max_retries_real    = pick($::sahara::database_max_retries, $database_max_retries)
-  $database_retry_interval_real = pick($::sahara::database_retry_interval, $database_retry_interval)
-  $database_max_overflow_real   = pick($::sahara::database_max_overflow, $database_max_overflow)
+  $database_connection_real              = pick($::sahara::database_connection, $database_connection)
+  $database_connection_recycle_time_real = pick($::sahara::database_idle_timeout, $database_idle_timeout, $database_connection_recycle_time)
+  $database_min_pool_size_real           = pick($::sahara::database_min_pool_size, $database_min_pool_size)
+  $database_max_pool_size_real           = pick($::sahara::database_max_pool_size, $database_max_pool_size)
+  $database_max_retries_real             = pick($::sahara::database_max_retries, $database_max_retries)
+  $database_retry_interval_real          = pick($::sahara::database_retry_interval, $database_retry_interval)
+  $database_max_overflow_real            = pick($::sahara::database_max_overflow, $database_max_overflow)
 
   validate_legacy(Oslo::Dbconn, 'validate_re', $database_connection_real,
     ['^(mysql(\+pymysql)?|postgresql):\/\/(\S+:\S+@\S+\/\S+)?'])
 
   oslo::db { 'sahara_config':
-    db_max_retries => $database_db_max_retries,
-    connection     => $database_connection_real,
-    idle_timeout   => $database_idle_timeout_real,
-    min_pool_size  => $database_min_pool_size_real,
-    max_pool_size  => $database_max_pool_size_real,
-    max_retries    => $database_max_retries_real,
-    retry_interval => $database_retry_interval_real,
-    max_overflow   => $database_max_overflow_real,
-    pool_timeout   => $database_pool_timeout,
+    db_max_retries          => $database_db_max_retries,
+    connection              => $database_connection_real,
+    connection_recycle_time => $database_connection_recycle_time_real,
+    min_pool_size           => $database_min_pool_size_real,
+    max_pool_size           => $database_max_pool_size_real,
+    max_retries             => $database_max_retries_real,
+    retry_interval          => $database_retry_interval_real,
+    max_overflow            => $database_max_overflow_real,
+    pool_timeout            => $database_pool_timeout,
   }
 }
--- spec/classes/sahara_db_spec.rb
+++ spec/classes/sahara_db_spec.rb
@@ -6,45 +6,45 @@ describe 'sahara::db' do
       it { should contain_class('sahara::deps') }
 
       it { is_expected.to contain_oslo__db('sahara_config').with(
-        :db_max_retries => '<SERVICE DEFAULT>',
-        :connection     => 'mysql+pymysql://sahara:secrete@localhost:3306/sahara',
-        :idle_timeout   => '<SERVICE DEFAULT>',
-        :min_pool_size  => '<SERVICE DEFAULT>',
-        :max_pool_size  => '<SERVICE DEFAULT>',
-        :max_retries    => '<SERVICE DEFAULT>',
-        :retry_interval => '<SERVICE DEFAULT>',
-        :max_overflow   => '<SERVICE DEFAULT>',
-        :pool_timeout   => '<SERVICE DEFAULT>',
+        :db_max_retries          => '<SERVICE DEFAULT>',
+        :connection              => 'mysql+pymysql://sahara:secrete@localhost:3306/sahara',
+        :connection_recycle_time => '<SERVICE DEFAULT>',
+        :min_pool_size           => '<SERVICE DEFAULT>',
+        :max_pool_size           => '<SERVICE DEFAULT>',
+        :max_retries             => '<SERVICE DEFAULT>',
+        :retry_interval          => '<SERVICE DEFAULT>',
+        :max_overflow            => '<SERVICE DEFAULT>',
+        :pool_timeout            => '<SERVICE DEFAULT>',
       )}
     end
 
     context 'with specific parameters' do
       let :params do
         {
-          :database_db_max_retries => '-1',
-          :database_connection     => 'mysql+pymysql://sahara:sahara@localhost/sahara',
-          :database_idle_timeout   => '3601',
-          :database_min_pool_size  => '2',
-          :database_max_retries    => '11',
-          :database_retry_interval => '11',
-          :database_max_pool_size  => '11',
-          :database_max_overflow   => '21',
-          :database_pool_timeout   => '21',
+          :database_db_max_retries          => '-1',
+          :database_connection              => 'mysql+pymysql://sahara:sahara@localhost/sahara',
+          :database_connection_recycle_time => '3601',
+          :database_min_pool_size           => '2',
+          :database_max_retries             => '11',
+          :database_retry_interval          => '11',
+          :database_max_pool_size           => '11',
+          :database_max_overflow            => '21',
+          :database_pool_timeout            => '21',
         }
       end
 
       it { should contain_class('sahara::deps') }
 
       it { is_expected.to contain_oslo__db('sahara_config').with(
-        :db_max_retries => '-1',
-        :connection     => 'mysql+pymysql://sahara:sahara@localhost/sahara',
-        :idle_timeout   => '3601',
-        :min_pool_size  => '2',
-        :max_pool_size  => '11',
-        :max_retries    => '11',
-        :retry_interval => '11',
-        :max_overflow   => '21',
-        :pool_timeout   => '21',
+        :db_max_retries          => '-1',
+        :connection              => 'mysql+pymysql://sahara:sahara@localhost/sahara',
+        :connection_recycle_time => '3601',
+        :min_pool_size           => '2',
+        :max_pool_size           => '11',
+        :max_retries             => '11',
+        :retry_interval          => '11',
+        :max_overflow            => '21',
+        :pool_timeout            => '21',
       )}
     end
   end
