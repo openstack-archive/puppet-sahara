11def8c8d96f934ea1ee3cdc89b63fddbdf579d3
Tobias Urdin <tobias.urdin@binero.se>
2020-01-14 10:54:20 +0100

    Remove sahara::service::all
    Remove the class to deploy all sahara services in favor
    of the specific classes for the API and engine.
    
    Example see removal from RDO here [1].
    
    [1] https://github.com/rdo-packages/sahara-distgit/commit/e5d05cc587b6b2a2f527480eefa8634e0d58202c
    
    Change-Id: I43cb3e526211faa2192b18287bfed62617dec4b2

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,11 +12,8 @@ class sahara::params {
   case $::osfamily {
     'RedHat': {
       $common_package_name       = 'openstack-sahara-common'
-      $all_package_name          = 'openstack-sahara'
       $api_package_name          = 'openstack-sahara-api'
       $engine_package_name       = 'openstack-sahara-engine'
-      # TODO(tobias-urdin): Remove this when deprecated sahara-all service is removed.
-      $all_service_name          = 'openstack-sahara-all'
       $api_service_name          = 'openstack-sahara-api'
       $engine_service_name       = 'openstack-sahara-engine'
       $sahara_wsgi_script_path   = '/var/www/cgi-bin/sahara'
@@ -24,10 +21,8 @@ class sahara::params {
     }
     'Debian': {
       $common_package_name       = 'sahara-common'
-      $all_package_name          = 'sahara'
       $api_package_name          = 'sahara-api'
       $engine_package_name       = 'sahara-engine'
-      $all_service_name          = 'sahara'
       $api_service_name          = 'sahara-api'
       $engine_service_name       = 'sahara-engine'
       $sahara_wsgi_script_path   = '/usr/lib/cgi-bin/sahara'
