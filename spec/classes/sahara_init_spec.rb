33eb7592023f51f4411fedbdf7f01bf7492c811a
ZhongShengping <chdzsp@163.com>
2020-11-02 14:42:04 +0800

    Deprecate allow_insecure_clients option
    The allow_insecure_clients has been deprecated[1].
    
    [1]https://review.opendev.org/#/c/417629/
    
    Change-Id: I6f0efafb8407ea6bc2fd9f06f4bce3b60f4c5342
    Closes-Bug: #1902158

--- spec/classes/sahara_init_spec.rb
+++ spec/classes/sahara_init_spec.rb
@@ -145,7 +145,6 @@ describe 'sahara' do
         it { is_expected.to contain_sahara_config('oslo_messaging_amqp/ssl_cert_file').with_value('<SERVICE DEFAULT>') }
         it { is_expected.to contain_sahara_config('oslo_messaging_amqp/ssl_key_file').with_value('<SERVICE DEFAULT>') }
         it { is_expected.to contain_sahara_config('oslo_messaging_amqp/ssl_key_password').with_value('<SERVICE DEFAULT>') }
-        it { is_expected.to contain_sahara_config('oslo_messaging_amqp/allow_insecure_clients').with_value('<SERVICE DEFAULT>') }
         it { is_expected.to contain_sahara_config('oslo_messaging_amqp/sasl_mechanisms').with_value('<SERVICE DEFAULT>') }
         it { is_expected.to contain_sahara_config('oslo_messaging_amqp/sasl_config_dir').with_value('<SERVICE DEFAULT>') }
         it { is_expected.to contain_sahara_config('oslo_messaging_amqp/sasl_config_name').with_value('<SERVICE DEFAULT>') }
