62390573327bd66c8c98137f1f7e4cd10fa8bf8a
Thomas Goirand <zigo@debian.org>
2021-04-01 23:28:22 +0200

    Allow to configure policy_dirs
    This patch makes it possible to override the current service default,
    which is /etc/<service>/policy.d .
    
    Change-Id: I645d57a9f79f77cf7540596845f9c94e8af4980f

--- spec/classes/sahara_policy_spec.rb
+++ spec/classes/sahara_policy_spec.rb
@@ -7,6 +7,7 @@ describe 'sahara::policy' do
         :enforce_scope        => false,
         :enforce_new_defaults => false,
         :policy_path          => '/etc/sahara/policy.yaml',
+        :policy_dirs          => '/etc/sahara/policy.d',
         :policies             => {
           'context_is_admin' => {
             'key'   => 'context_is_admin',
@@ -28,6 +29,7 @@ describe 'sahara::policy' do
         :enforce_scope        => false,
         :enforce_new_defaults => false,
         :policy_file          => '/etc/sahara/policy.yaml',
+        :policy_dirs          => '/etc/sahara/policy.d',
       )
     end
   end
