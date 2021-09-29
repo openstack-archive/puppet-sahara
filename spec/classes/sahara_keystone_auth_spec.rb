97d289af05470f679d46e805930cc3346371fee4
Takashi Kajinami <tkajinam@redhat.com>
2021-09-29 13:12:29 +0900

    Do not test detail of keystone::resource::service_identity
    ... but test interfaces of that resource type. This helps us avoid
    direct breakage caused by any change in puppet-keystone.
    
    Change-Id: I17d8c3c5e0d2ffed0c84d1bf26c027b239f09be3

--- spec/classes/sahara_keystone_auth_spec.rb
+++ spec/classes/sahara_keystone_auth_spec.rb
@@ -1,89 +1,78 @@
 #
 # Unit tests for sahara::keystone::auth
 #
+
 require 'spec_helper'
 
 describe 'sahara::keystone::auth' do
-
-  shared_examples_for 'sahara-keystone-auth' do
+  shared_examples_for 'sahara::keystone::auth' do
     context 'with default class parameters' do
       let :params do
-        { :password => 'sahara_password',
-          :tenant   => 'foobar' }
+        { :password => 'sahara_password' }
       end
 
-      it { is_expected.to contain_keystone_user('sahara').with(
-        :ensure   => 'present',
-        :password => 'sahara_password',
-      ) }
-
-      it { is_expected.to contain_keystone_user_role('sahara@foobar').with(
-        :ensure  => 'present',
-        :roles   => ['admin']
-      )}
-
-      it { is_expected.to contain_keystone_service('sahara::data-processing').with(
-        :ensure      => 'present',
-        :description => 'Sahara Data Processing'
-      ) }
-
-      it { is_expected.to contain_keystone_endpoint('RegionOne/sahara::data-processing').with(
-        :ensure       => 'present',
-        :public_url   => "http://127.0.0.1:8386",
-        :admin_url    => "http://127.0.0.1:8386",
-        :internal_url => "http://127.0.0.1:8386"
+      it { is_expected.to contain_keystone__resource__service_identity('sahara').with(
+        :configure_user      => true,
+        :configure_user_role => true,
+        :configure_endpoint  => true,
+        :service_name        => 'sahara',
+        :service_type        => 'data-processing',
+        :service_description => 'Sahara Data Processing',
+        :region              => 'RegionOne',
+        :auth_name           => 'sahara',
+        :password            => 'sahara_password',
+        :email               => 'sahara@localhost',
+        :tenant              => 'services',
+        :public_url          => 'http://127.0.0.1:8386',
+        :internal_url        => 'http://127.0.0.1:8386',
+        :admin_url           => 'http://127.0.0.1:8386',
       ) }
     end
 
-    context 'when configuring sahara-server' do
-      let :pre_condition do
-        "class { 'sahara::server': auth_password => 'test' }"
-      end
-
+    context 'when overriding parameters' do
       let :params do
-        { :password => 'sahara_password',
-          :tenant   => 'foobar' }
+        { :password            => 'sahara_password',
+          :auth_name           => 'alt_sahara',
+          :email               => 'alt_sahara@alt_localhost',
+          :tenant              => 'alt_service',
+          :configure_endpoint  => false,
+          :service_description => 'Alternative Sahara Data Processing',
+          :service_name        => 'alt_service',
+          :service_type        => 'alt_data-processing',
+          :region              => 'RegionTwo',
+          :public_url          => 'https://10.10.10.10:80',
+          :internal_url        => 'http://10.10.10.11:81',
+          :admin_url           => 'http://10.10.10.12:81' }
       end
-    end
 
-    context 'with endpoint parameters' do
-      let :params do
-        { :password     => 'sahara_password',
-          :public_url   => 'https://10.10.10.10:80',
-          :internal_url => 'http://10.10.10.11:81',
-          :admin_url    => 'http://10.10.10.12:81' }
-      end
-
-      it { is_expected.to contain_keystone_endpoint('RegionOne/sahara::data-processing').with(
-        :ensure       => 'present',
-        :public_url   => 'https://10.10.10.10:80',
-        :internal_url => 'http://10.10.10.11:81',
-        :admin_url    => 'http://10.10.10.12:81'
+      it { is_expected.to contain_keystone__resource__service_identity('sahara').with(
+        :configure_user      => true,
+        :configure_user_role => true,
+        :configure_endpoint  => false,
+        :service_name        => 'alt_service',
+        :service_type        => 'alt_data-processing',
+        :service_description => 'Alternative Sahara Data Processing',
+        :region              => 'RegionTwo',
+        :auth_name           => 'alt_sahara',
+        :password            => 'sahara_password',
+        :email               => 'alt_sahara@alt_localhost',
+        :tenant              => 'alt_service',
+        :public_url          => 'https://10.10.10.10:80',
+        :internal_url        => 'http://10.10.10.11:81',
+        :admin_url           => 'http://10.10.10.12:81',
       ) }
-    end
-
-    context 'when overriding auth name' do
-      let :params do
-        { :password => 'foo',
-          :auth_name => 'saharay' }
-      end
-
-      it { is_expected.to contain_keystone_user('saharay') }
-      it { is_expected.to contain_keystone_user_role('saharay@services') }
-      it { is_expected.to contain_keystone_service('sahara::data-processing') }
-      it { is_expected.to contain_keystone_endpoint('RegionOne/sahara::data-processing') }
     end
   end
 
   on_supported_os({
-    :supported_os   => OSDefaults.get_supported_os
+    :supported_os => OSDefaults.get_supported_os
   }).each do |os,facts|
     context "on #{os}" do
       let (:facts) do
-        facts.merge(OSDefaults.get_facts)
+        facts.merge!(OSDefaults.get_facts())
       end
-      it_configures 'sahara-keystone-auth'
+
+      it_behaves_like 'sahara::keystone::auth'
     end
   end
-
 end
