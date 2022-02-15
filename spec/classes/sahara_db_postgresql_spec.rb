8b78315331b4db67a6f788afd29bce12fe332db8
Takashi Kajinami <tkajinam@redhat.com>
2022-02-16 00:28:26 +0900

    CentOS 9: Disable unit tests dependent on puppet-postgresql
    The puppt-postgresql module does not support CentOS 9 yet and requires
    some version parameters to be run on CentOS 9. This change disables
    unit tests requiring that module, until the module supports CentOS 9.
    
    Change-Id: I6add22ed438d2cec0a036d90c5c5ea7de1653533

--- spec/classes/sahara_db_postgresql_spec.rb
+++ spec/classes/sahara_db_postgresql_spec.rb
@@ -39,7 +39,10 @@ describe 'sahara::db::postgresql' do
         }))
       end
 
-      it_configures 'sahara::db::postgresql'
+      # TODO(tkajinam): Remove this once puppet-postgresql supports CentOS 9
+      unless facts[:osfamily] == 'RedHat' and facts[:operatingsystemmajrelease].to_i >= 9
+        it_configures 'sahara::db::postgresql'
+      end
     end
   end
 
