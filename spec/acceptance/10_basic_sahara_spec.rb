f92533696c3979ba3b8a84f4855a379ac82845b5
Takashi Kajinami <tkajinam@redhat.com>
2022-09-07 01:01:02 +0900

    Enable memcached in acceptance tests
    ... because it is required as cache backend.
    
    Change-Id: Ibb9ef40fc3649fc0b31074258e46c01450b1d817

--- spec/acceptance/10_basic_sahara_spec.rb
+++ spec/acceptance/10_basic_sahara_spec.rb
@@ -11,6 +11,7 @@ describe 'basic sahara' do
       include openstack_integration::apache
       include openstack_integration::rabbitmq
       include openstack_integration::mysql
+      include openstack_integration::memcached
       include openstack_integration::keystone
       class { 'openstack_integration::sahara':
         integration_enable => false,
