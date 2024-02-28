50b3fc6ac73935ef04c3af014d94b26d429287d5
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-02-28 10:55:28 +0900

    Retire puppet-sahara: Remove Project Content
    Depends-on: https://review.opendev.org/c/openstack/project-config/+/910452
    Change-Id: Ifbcdf71f515ab24da67c9350b958cb987b1badc8

--- lib/puppet/provider/sahara_api_uwsgi_config/ini_setting.rb
+++
@@ -1,8 +0,0 @@
-Puppet::Type.type(:sahara_api_uwsgi_config).provide(
-  :ini_setting,
-  :parent => Puppet::Type.type(:openstack_config).provider(:ini_setting)
-) do
-  def self.file_path
-    '/etc/sahara/sahara-api-uwsgi.ini'
-  end
-end
